0dc80943859acdce4aa2752d835c232f2b1b31bf
Blake Erickson <blake@discourse.org>
2025-04-28 09:47:52 -0600

    UX: Hide search (#2)
    Since we don't have the ability to search messages in chat yet let's
    hide the search buttons for now.
    
    https://meta.discourse.org/t/ability-to-search-messages-in-chat/240803

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # name: discourse-just-chat
-# about:  Hides everything that isn't chat
+# about: Hides everything that isn't chat
 # version: 0.0.1
 # authors: Blake Erickson
 # url: https://github.com/discourse/discourse-just-chat
